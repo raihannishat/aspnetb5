e5cf031e5fe2c4425bc60bd3cd2bb44f01cb693b
raihannishat <raihannishat.swe@gmail.com>
2021-05-01 21:50:05 +0600

    Configure MVC Project

---
+++ Explore/Explore/Controllers/DateTimeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Explore.Models;
+
+namespace Explore.Controllers
+{
+    public class DateTimeController : Controller
+    {
+        public IActionResult GetDateTime()
+        {
+            return View(new DateTimeModel());
+        }
+    }
+}
---
+++ Explore/Explore/Models/SmtpConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Explore.Models
+{
+    public class SmtpConfiguration
+    {
+        public string Host { get; set; }
+        public string Username { get; set; }
+        public string Pasword { get; set; }
+        public string Port { get; set; }
+    }
+}
