f358de30226799c29ddf03f7b30892edb809d83b
raihannishat <raihannishat.swe@gmail.com>
2021-09-30 16:32:39 +0600

    This is problem

--- DataImporter/DataImporter.Data/IEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DataImporter.Data
-{
-    public interface IEntity<T>
-    {   
-        T Id { get; set; }  
-    }
-}
--- DataImporter/DataImporter.Data/IUnitOfWork.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DataImporter.Data
-{
-    public interface IUnitOfWork : IDisposable
-    {
-        void Save();
-    }
-}
