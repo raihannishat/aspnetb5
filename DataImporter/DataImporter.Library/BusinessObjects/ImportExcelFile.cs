0b1a235feb0b6fc4e0e6b9691b49b8c6c0520a11
raihannishat <raihannishat.swe@gmail.com>
2021-10-02 16:35:59 +0600

    add a new Business Object

---
+++ DataImporter/DataImporter.Library/BusinessObjects/ImportExcelFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter.Library.BusinessObjects
+{
+    public class ImportExcelFile
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public Guid UserId { get; set; }
+        public string Location { get; set; }
+        public DateTime ImportDate { get; set; }
+        public string ImportStatus { get; set; }
+    }
+}
