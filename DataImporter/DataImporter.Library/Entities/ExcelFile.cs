83c3129c2d953eea644a907cc5ec9b00e05fa464
raihannishat <raihannishat.swe@gmail.com>
2021-10-02 16:27:42 +0600

    add new Entities

--- DataImporter/DataImporter.Library/Entities/ExcelFile.cs
+++ DataImporter/DataImporter.Library/Entities/ExcelFile.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataImporter.Data;
+using DataImporter.Membership.Entities;
 
 namespace DataImporter.Library.Entities
 {
@@ -11,7 +12,10 @@ namespace DataImporter.Library.Entities
     {
         public int Id { get; set; }
         public int GroupId { get; set; }
-        public int UserId { get; set; }
+        public Group Group { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; }
         public DateTime UploadDate { get; set; }
+        public List<ExcelColumn> ExcelColumns { get; set; }
     }
 }
--- DataImporter/DataImporter.Library/Entities/Group.cs
+++ DataImporter/DataImporter.Library/Entities/Group.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataImporter.Data;
+using DataImporter.Membership.Entities;
 
 namespace DataImporter.Library.Entities
 {
@@ -11,5 +12,9 @@ namespace DataImporter.Library.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public Guid UserId { get; set; }
+        public ApplicationUser User { get; set; }
+        public List<ExcelFile> ExcelFiles { get; set; }
+        public List<History> Histories { get; set; }
     }
 }
