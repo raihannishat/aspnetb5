8f1e5b7cd396192179d0135314a14f6f77935cad
raihannishat <raihannishat.swe@gmail.com>
2021-10-02 16:37:13 +0600

    add new Repositories for this project

---
+++ DataImporter/DataImporter.Library/Repositories/ExportExcelFileRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataImporter.Data;
+using DataImporter.Library.Entities;
+using DataImporter.Library.Contexts;
+
+namespace DataImporter.Library.Repositories
+{
+    public class ExportExcelFileRepository : Repository<ExportExcelFile, int>, IExportExcelFileRepository
+    {
+        public ExportExcelFileRepository(IDataImporterDbContext context) : base((Context)context)
+        {
+
+        }
+    }
+}
