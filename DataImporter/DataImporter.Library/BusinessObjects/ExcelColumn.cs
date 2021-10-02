1215748a975583b7f6682f9aa457e477c91e1110
raihannishat <raihannishat.swe@gmail.com>
2021-10-02 16:32:33 +0600

    add new Business Objects Class

---
+++ DataImporter/DataImporter.Library/BusinessObjects/ExcelColumn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter.Library.BusinessObjects
+{
+    public class ExcelColumn
+    {
+        public int Id { get; set; }
+        public string Column { get; set; }
+        public string Value { get; set; }
+        public int ExcelFileId { get; set; }
+    }
+}
---
+++ DataImporter/DataImporter.Library/BusinessObjects/ExportExcelFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter.Library.BusinessObjects
+{
+    public class ExportExcelFile
+    {
+        public int Id { get; set; }
+        public Guid UserId { get; set; }
+        public int GroupId { get; set; }
+        public DateTime ExportDate { get; set; }
+    }
+}
