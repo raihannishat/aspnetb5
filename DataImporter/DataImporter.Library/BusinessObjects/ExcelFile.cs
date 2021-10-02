54a962e4c97a3c22a01b3e973493507bf55b0e8a
raihannishat <raihannishat.swe@gmail.com>
2021-10-02 16:27:25 +0600

    add new Business Objects

--- DataImporter/DataImporter.Library/BusinessObjects/ExcelFile.cs
+++ DataImporter/DataImporter.Library/BusinessObjects/ExcelFile.cs
@@ -8,5 +8,9 @@ namespace DataImporter.Library.BusinessObjects
 {
     public class ExcelFile
     {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime UploadDate { get; set; }
     }
 }
--- DataImporter/DataImporter.Library/BusinessObjects/Group.cs
+++ DataImporter/DataImporter.Library/BusinessObjects/Group.cs
@@ -8,5 +8,8 @@ namespace DataImporter.Library.BusinessObjects
 {
     public class Group
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Guid UserId { get; set; }
     }
 }
