47d35c9b46eedcaf9c6dd7ab014f27c4b239df79
raihannishat <raihannishat.swe@gmail.com>
2021-10-04 12:29:14 +0600

    Add new model for GoogleReCAPTCHA

---
+++ DataImporter/DataImporter.Web/Models/GoogleReCAPTCHA/GoogleReCaptchaService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DataImporter.Web.Models.GoogleReCAPTCHA
+{
+    public class GoogleReCaptchaService
+    {
+        public GoogleReCaptchaService(IOptions<ReCAPTCHASettings> settings)
+        {
+            Settings = settings.Value;
+        }
+
+        private ReCAPTCHASettings Settings { get; }
+
+        public virtual async Task<GoogleResponseData> VerifyReCaptcha(string token)
+        {
+            GoogleCaptchaData data = new GoogleCaptchaData
+            {
+                Response = token,
+                Secret = Settings.ReCAPTCHA_Secret_key
+            };
+
+            using var client = new HttpClient();
+
+            var response = await client.GetStringAsync($"https://www.google.com/recaptcha/api/siteverify?secret={data.Secret}&response={data.Response}");
+
+            var captcha = JsonConvert.DeserializeObject<GoogleResponseData>(response);
+
+            return captcha;
+        }
+    }
+
+    public class GoogleCaptchaData
+    {
+        public string Response { get; set; }
+        public string Secret { get; set; }
+    }
+
+    public class GoogleResponseData
+    {
+        public bool Success { get; set; }
+        public double Score { get; set; }
+        public string Action { get; set; }
+        public DateTime Challenge_ts { get; set; }
+        public string HostName { get; set; }
+    }
+}
