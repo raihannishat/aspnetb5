4c5f54b7a3b6abd42e49ec08b155ef7c49f38df7
raihannishat <raihannishat.swe@gmail.com>
2021-10-05 12:24:13 +0600

    this is new BusinessObject

---
+++ DataImporter/DataImporter.Library/BusinessObjects/ExcelRow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImporter.Library.BusinessObjects
+{
+    public class ExcelRow
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public DateTime UploadDate { get; set; }
+    }
+}
