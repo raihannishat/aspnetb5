c876022e52aa1a51a516df48198ee0266c869e8c
raihannishat <raihannishat.swe@gmail.com>
2021-10-05 12:27:02 +0600

    add new Entity class

---
+++ DataImporter/DataImporter.Library/Entities/ExcelRow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataImporter.Data;
+using DataImporter.Membership.Entities;
+
+namespace DataImporter.Library.Entities
+{
+    public class ExcelRow : IEntity<int>
+    {
+        public int Id { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+        public DateTime UploadDate { get; set; }
+        public List<ExcelColumn> ExcelColumns { get; set; }
+    }
+}
