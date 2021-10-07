5acab7d1b43371f71080d7a3eea05150b9323175
raihannishat <raihannishat.swe@gmail.com>
2021-10-07 17:32:46 +0600

    automapper not working for this project

--- DataImporter/DataImporter.Worker/Program.cs
+++ DataImporter/DataImporter.Worker/Program.cs
@@ -1,24 +1,78 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using DataImporter.Library.Contexts;
+using DataImporter.Library;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataImporter.Worker
 {
     public class Program
     {
+        private static IConfiguration _configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .AddEnvironmentVariables()
+                .Build();
+
+        private static string _connectionString = _configuration
+            .GetConnectionString("DefaultConnection");
+
+        private static string _migrationAssembly = Assembly.GetExecutingAssembly()
+            .GetName().FullName;
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .ReadFrom.Configuration(_configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Worker Starting up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Worker start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .UseSerilog()
+                .ConfigureContainer<ContainerBuilder>(builder => {
+                    builder.RegisterModule(new LibraryModule(_connectionString,
+                        _migrationAssembly));
+
+                    builder.RegisterModule(new WorkerModule(_configuration,
+                        _connectionString,
+                        _migrationAssembly));
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
+
+                    services.AddDbContext<DataImporterDbContext>(options =>
+                        options.UseSqlServer(_connectionString,
+                        m => m.MigrationsAssembly(_migrationAssembly)));
                 });
     }
 }
--- DataImporter/DataImporter.Worker/Worker.cs
+++ DataImporter/DataImporter.Worker/Worker.cs
@@ -5,23 +5,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using DataImporter.Worker.Model;
+using Autofac;
 
 namespace DataImporter.Worker
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        public static ILifetimeScope AutofacContainer { get; private set; }
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, ILifetimeScope lifetimeScope)
         {
             _logger = logger;
+            AutofacContainer = lifetimeScope;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                //_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+                var importer = new WorkerModel();
+                importer.GetAllStatus();
+
                 await Task.Delay(1000, stoppingToken);
             }
         }
