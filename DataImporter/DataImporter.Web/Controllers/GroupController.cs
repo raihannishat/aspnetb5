903e8a005ec9ced9f67ab518ba8fdf751c1da096
raihannishat <raihannishat.swe@gmail.com>
2021-10-07 17:34:07 +0600

    problem fix

--- DataImporter/DataImporter.Web/Controllers/GroupController.cs
+++ DataImporter/DataImporter.Web/Controllers/GroupController.cs
@@ -1,18 +1,99 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DataImporter.Web.Models.Groups;
+using DataImporter.Core;
+using System.Security.Claims;
 
 namespace DataImporter.Web.Controllers
 {
     [Authorize(Roles = "User")]
     public class GroupController : Controller
     {
+        private readonly ILogger<GroupController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public GroupController(ILogger<GroupController> logger, 
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult GetGroupData()
+        {
+            var dataTablesModel = new DataTablesAjaxRequestModel(Request);
+            var model = new GroupListModel();
+            var data = model.GetGroups(dataTablesModel);
+            return Json(data);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CreateGroupModel());
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create(CreateGroupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.ApplicationUserId = Guid.Parse(_httpContextAccessor
+                        .HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+                    model.Create();
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("", "Failed to create group");
+                    _logger.LogError(ex, "Create Group Failed");
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var model = new EditGroupModel();
+            model.LoadModelData(id);
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit(EditGroupModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.ApplicationUserId = Guid.Parse(_httpContextAccessor
+                    .HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value);
+                model.Update();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var model = new GroupListModel();
+            model.Delete(id);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- DataImporter/DataImporter.Web/Controllers/ImportController.cs
+++ DataImporter/DataImporter.Web/Controllers/ImportController.cs
@@ -1,18 +1,105 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DataImporter.Web.Models.Imports;
+using Microsoft.Extensions.Logging;
+using DataImporter.Core;
 
 namespace DataImporter.Web.Controllers
 {
     [Authorize(Roles = "User")]
     public class ImportController : Controller
     {
+        private readonly ILogger<ImportController> _logger;
+
+        public ImportController(ILogger<ImportController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult ImportHistory()
+        {
+            return View();
+        }
+
+        public IActionResult GetImportData()
+        {
+            var dataTablesModel = new DataTablesAjaxRequestModel(Request);
+            var model = new ImportListModel();
+            var data = model.GetImpots(dataTablesModel);
+            return Json(data);
+        }
+
+        [HttpGet]
+        public IActionResult UploadExcelFile(int id)
+        {
+            var model = new CreateImportModel();
+            model.GroupId = id;
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> UploadExcelFile(IFormFile file, CreateImportModel model)
+        {
+            await UploadFile(file, model);
+            return RedirectToAction(nameof(ImportHistory));
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<bool> UploadFile(IFormFile file, CreateImportModel model)
+        {
+            string path = null;
+            bool copy = false;
+
+            try
+            {
+                if (file.Length > 0)
+                {
+                    string fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                    path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "Upload"));
+
+                    using (var stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                        TempData["msg"] = "File upload successful";
+                        model.Location = Path.GetFullPath(path) + "\\" + fileName;
+                        model.ImportStatus = "Working";
+                        model.ImportDate = DateTime.Now;
+                        model.Create();
+                    }
+
+                    copy = true;
+                }
+                else
+                {
+                    copy = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(string.Empty, ex.Message);
+            }
+
+            return copy;
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            var model = new ImportListModel();
+            model.Delete(id);
+
+            return RedirectToAction(nameof(ImportHistory));
         }
     }
 }
