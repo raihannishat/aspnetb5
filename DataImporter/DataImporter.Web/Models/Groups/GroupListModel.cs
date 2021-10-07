49894c304fac6e5dccc13962008b121993d0028c
raihannishat <raihannishat.swe@gmail.com>
2021-10-07 19:57:12 +0600

    update for user manage

--- DataImporter/DataImporter.Web/Models/Groups/GroupListModel.cs
+++ DataImporter/DataImporter.Web/Models/Groups/GroupListModel.cs
@@ -1,34 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Autofac;
 using DataImporter.Core;
 using DataImporter.Library.Services;
+using Microsoft.AspNetCore.Http;
 
 namespace DataImporter.Web.Models.Groups
 {
     public class GroupListModel
     {
         private readonly IGroupService _groupService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public GroupListModel()
         {
             _groupService = Startup.AutofacContainer.Resolve<IGroupService>();
+            _httpContextAccessor = Startup.AutofacContainer.Resolve<IHttpContextAccessor>();
         }
 
-        public GroupListModel(IGroupService groupService)
+        public GroupListModel(IGroupService groupService, IHttpContextAccessor httpContextAccessor)
         {
             _groupService = groupService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public object GetGroups(DataTablesAjaxRequestModel tableModel)
         {
+            var userId = Guid.Parse(_httpContextAccessor.HttpContext.User
+                        .FindFirst(ClaimTypes.NameIdentifier).Value);
+
             var data = _groupService.GetGroups(
                 tableModel.PageIndex,
                 tableModel.PageSize,
                 tableModel.SearchText,
-                tableModel.GetSortText(new string[] { "Id", "Name" }));
+                tableModel.GetSortText(new string[] { "Id", "Name" }), userId);
 
             return new
             {
--- DataImporter/DataImporter.Web/Models/Imports/ImportListModel.cs
+++ DataImporter/DataImporter.Web/Models/Imports/ImportListModel.cs
@@ -1,34 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Autofac;
 using DataImporter.Core;
 using DataImporter.Library.Services;
+using Microsoft.AspNetCore.Http;
 
 namespace DataImporter.Web.Models.Imports
 {
     public class ImportListModel
     {
         private readonly IImportExcelFileService _importExcelFileService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public ImportListModel()
         {
             _importExcelFileService = Startup.AutofacContainer.Resolve<IImportExcelFileService>();
+            _httpContextAccessor = Startup.AutofacContainer.Resolve<IHttpContextAccessor>();
         }
 
-        public ImportListModel(IImportExcelFileService importExcelFileService)
+        public ImportListModel(IImportExcelFileService importExcelFileService,
+            IHttpContextAccessor httpContextAccessor)
         {
             _importExcelFileService = importExcelFileService;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public object GetImpots(DataTablesAjaxRequestModel tableModel)
         {
+            var userId = Guid.Parse(_httpContextAccessor.HttpContext.User
+                        .FindFirst(ClaimTypes.NameIdentifier).Value);
+
             var data = _importExcelFileService.GetImportExcelFiles(
                 tableModel.PageIndex,
                 tableModel.PageSize,
                 tableModel.SearchText,
-                tableModel.GetSortText(new string[] { "Id", "Name" }));
+                tableModel.GetSortText(new string[] { "Id", "Name" }), userId);
 
             return new
             {
